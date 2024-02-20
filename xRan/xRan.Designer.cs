8d625187ce8b714bfef390a28c59a4896b729251
abdulraheemgit <abdulraheemedu@gmail.com>
2024-02-20 19:48:36 +0000

    code commit

---
+++ xRan/xRan.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace xRan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblCredits = new System.Windows.Forms.Label();
+            this.lblDesc6 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblChallenge = new System.Windows.Forms.Label();
+            this.lblDesc2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.lblresult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::xRan.Properties.Resources._1;
+            this.pictureBox3.Location = new System.Drawing.Point(844, 26);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(157, 126);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox11.Image = global::xRan.Properties.Resources.ran1;
+            this.pictureBox11.Location = new System.Drawing.Point(1003, 1);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(303, 158);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox11.TabIndex = 11;
+            this.pictureBox11.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(156, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(425, 47);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "hey   this   is   xRansomware\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(207, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 47);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "your flag is encrypted\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DimGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(820, 500);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 26);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(29)))), ((int)(((byte)(15)))));
+            this.label3.Location = new System.Drawing.Point(702, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 64);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "by";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(29)))), ((int)(((byte)(15)))));
+            this.label4.Location = new System.Drawing.Point(73, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(662, 170);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Encrypted\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Georgia", 150F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(29)))), ((int)(((byte)(15)))));
+            this.label5.Location = new System.Drawing.Point(-16, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 227);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "!\r\n";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label7.Location = new System.Drawing.Point(178, 357);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(381, 47);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "we  got  a  news  for  you";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Segoe Print", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label8.Location = new System.Drawing.Point(312, 496);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 61);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "oops!";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Salmon;
+            this.label9.Location = new System.Drawing.Point(590, 583);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(613, 15);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Please Do Not Turn Off your computer. Your decryption key totally depend on keepi" +
+    "ng the computer switched on.";
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.AutoSize = true;
+            this.lblCredits.BackColor = System.Drawing.Color.Transparent;
+            this.lblCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCredits.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblCredits.Location = new System.Drawing.Point(1081, 641);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(213, 12);
+            this.lblCredits.TabIndex = 22;
+            this.lblCredits.Text = "Developed By AR Powered by Sri Lanka CERT|CC";
+            // 
+            // lblDesc6
+            // 
+            this.lblDesc6.AutoSize = true;
+            this.lblDesc6.BackColor = System.Drawing.Color.Transparent;
+            this.lblDesc6.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc6.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblDesc6.Location = new System.Drawing.Point(948, 419);
+            this.lblDesc6.Name = "lblDesc6";
+            this.lblDesc6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblDesc6.Size = new System.Drawing.Size(252, 36);
+            this.lblDesc6.TabIndex = 23;
+            this.lblDesc6.Text = "The encrypted flag (flag.txt.xRan) is \r\nsaved in the xRan folder";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(1074, 481);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(126, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Enter key to decrypt";
+            // 
+            // lblChallenge
+            // 
+            this.lblChallenge.AutoSize = true;
+            this.lblChallenge.BackColor = System.Drawing.Color.Transparent;
+            this.lblChallenge.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChallenge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(29)))), ((int)(((byte)(15)))));
+            this.lblChallenge.Location = new System.Drawing.Point(805, 280);
+            this.lblChallenge.Name = "lblChallenge";
+            this.lblChallenge.Size = new System.Drawing.Size(265, 38);
+            this.lblChallenge.TabIndex = 24;
+            this.lblChallenge.Text = "Up for a challenge?";
+            // 
+            // lblDesc2
+            // 
+            this.lblDesc2.AutoSize = true;
+            this.lblDesc2.BackColor = System.Drawing.Color.Transparent;
+            this.lblDesc2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc2.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lblDesc2.Location = new System.Drawing.Point(920, 353);
+            this.lblDesc2.Name = "lblDesc2";
+            this.lblDesc2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblDesc2.Size = new System.Drawing.Size(281, 46);
+            this.lblDesc2.TabIndex = 25;
+            this.lblDesc2.Text = "Your flag is encrypted\r\nDecrypt the file and win the flag";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(591, 567);
+            this.label10.Name = "label10";
+            this.label10.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label10.Size = new System.Drawing.Size(49, 16);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "NOTE:";
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.DimGray;
+            this.Button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button1.Location = new System.Drawing.Point(1077, 532);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(120, 23);
+            this.Button1.TabIndex = 27;
+            this.Button1.Text = "Decrypt";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.BackColor = System.Drawing.Color.Transparent;
+            this.lblresult.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(29)))), ((int)(((byte)(15)))));
+            this.lblresult.Location = new System.Drawing.Point(1074, 562);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 16);
+            this.lblresult.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::xRan.Properties.Resources.bg3;
+            this.ClientSize = new System.Drawing.Size(1295, 656);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblDesc2);
+            this.Controls.Add(this.lblChallenge);
+            this.Controls.Add(this.lblDesc6);
+            this.Controls.Add(this.lblCredits);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "xRan";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblCredits;
+        private System.Windows.Forms.Label lblDesc6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblChallenge;
+        private System.Windows.Forms.Label lblDesc2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ xRan/xRan.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xRan
+{
+    public partial class Form1 : Form
+    {
+        static int tc;
+        public Form1()
+        {
+            tc = Environment.TickCount & Int32.MaxValue;
+            startAction();
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            InitializeComponent();
+        }
+        public void startAction()
+        {
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            CompilerParameters parameters = new CompilerParameters();
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.GenerateExecutable = true;
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.GenerateInMemory = true;
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.TreatWarningsAsErrors = false;
+            parameters.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.ReferencedAssemblies.Add("System.dll");
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.ReferencedAssemblies.Add("System.Drawing.dll");
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.ReferencedAssemblies.Add("System.Linq.dll");
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            parameters.ReferencedAssemblies.Add("System.Data.dll");
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            CompilerResults result = provider.CompileAssemblyFromSource(parameters, k());            
+            if (result.Errors.Count > 0)
+            {
+                foreach (CompilerError er in result.Errors)
+                    Console.WriteLine(er.ToString());
+                return;
+            }
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            Assembly assembly = result.CompiledAssembly;
+            MethodInfo methodInfo = assembly.EntryPoint;
+            object entryPointInstance = assembly.CreateInstance(methodInfo.Name);
+            methodInfo.Invoke(entryPointInstance, null);
+        }
+                
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string key = this.textBox1.Text;
+            string path = "xRan/flag.txt.xRan";
+            tc = Environment.TickCount & Int32.MaxValue;
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            if (File.Exists(path))
+            {                
+                i(path, key);
+                lblresult.Text = "File Decrypted";
+            }
+            else
+            {
+                lblresult.Text = "File Not Found!";
+            }
+        }
+        public void i(string file, string password)
+        {
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(file);
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            byte[] bytesDecrypted = h(bytesToBeDecrypted, SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(password)));
+            File.WriteAllBytes(file, bytesDecrypted);
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            string extension = System.IO.Path.GetExtension(file);
+            string result = file.Substring(0, file.Length - extension.Length);
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000) { Environment.Exit(1); }
+            else { tc = Environment.TickCount & Int32.MaxValue; }
+            System.IO.File.Move(file, result);
+
+        }
+        public byte[] h(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            if ((Environment.TickCount & Int32.MaxValue - tc) > 2000){Environment.Exit(1);}
+            else{tc = Environment.TickCount & Int32.MaxValue;}
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+            return decryptedBytes;
+        }
+        static String[] k()
+        {
+            byte[] bytes = File.ReadAllBytes(Assembly.GetEntryAssembly().Location);
+
+            int i = 0;
+            string y = "RkBoNZ42gBQwdhiyQ5y0GVIDY7FKEcVb";
+            string key = "JXR7svy0LQYAio2pqyxrHQOYHVzXJdiN";
+            string pass = "RmSeZBUGeENW9pZDROzlHtbp9VAQ9jmD";
+
+
+            List<String> sourceFiles = new List<String>();
+
+            for (i = 0; i < bytes.Length - 2; i++)
+            {
+                if (bytes[i] == bytes[i + 1] && bytes[i + 1] == bytes[i + 2] && bytes[i + 2] == 28)
+                {
+                    i += 3;
+                    break;
+                }
+            }
+            List<Byte> sourceFileBuffer = new List<Byte>(4000);
+            for (; i < bytes.Length - 2; i++)
+            {
+                if (bytes[i] == bytes[i + 1] && bytes[i + 1] == bytes[i + 2] && bytes[i + 2] == 28)
+                {
+                    sourceFiles.Add(Encoding.Default.GetString(j(sourceFileBuffer.ToArray(), y)));
+                    sourceFileBuffer.Clear();
+                    i += 2;
+                }
+                else
+                    sourceFileBuffer.Add(bytes[i]);
+
+            }
+            return sourceFiles.ToArray();
+        }
+        public static byte[] j(byte[] text, string key)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.KeySize = 256;
+            aes.BlockSize = 256;
+            aes.Padding = PaddingMode.Zeros;
+            aes.Mode = CipherMode.CBC;
+            aes.Key = Encoding.Default.GetBytes(key);
+            byte[] IV = new byte[32];
+            Array.Copy(text, text.Length - 32, IV, 0, 32);
+            byte[] text2 = new byte[text.Length - 32];
+            Array.Copy(text, text2, text2.Length);
+            aes.IV = IV;
+            ICryptoTransform AESDecrypt = aes.CreateDecryptor(aes.Key, aes.IV);
+            return AESDecrypt.TransformFinalBlock(text2, 0, text2.Length);
+        }
+    }
+}
